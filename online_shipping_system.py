dcedc29acd19d095204215f20b7ccda1acd24ef2
Maxwell <codingxam@gmail.com>
2024-01-06 01:13:03 -0400

    culminated online shipping system

--- online_shipping_system.py
+++ online_shipping_system.py
@@ -6,3 +6,75 @@
 # * 		The system calculates the shipping price. $2 per kg.
 # * 		The user must input the total weight of their package, and the system should display the amount to pay.
 # * 		The system should ask if the user wants to perform another operation. If the answer is yes, it should return to the main menu. If it's no, it should close the system.
+
+import os
+import time
+import random
+
+users = [{
+    "username": "admin",
+    "password": "123456",
+    "balance": 2000,
+}]
+
+
+def clear():
+    return os.system("cls" if os.name == "nt" else "clear")
+
+
+
+def login(username, password):
+    for user in users:
+        if user["username"] == username and user["password"] == password:
+            return True
+    return False
+
+def main():
+    clear()
+    options = ['Sending a package', 'exit']
+    while True:
+        clear()
+        attempts = 0
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        if login(username, password):
+            print("Login successful")
+            break
+        else:
+            print("Login failed")
+            attempts += 1
+            if attempts == 3:
+                print("System locked")
+                break
+    while True:
+        clear()
+        print("Welcome,", username)
+        for i, option in enumerate(options):
+            print(f"{i+1}. {option}")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            sender = input("Enter sender name: ")
+            recipient = input("Enter recipient name: ")
+            weight = int(input("Enter weight of package: "))
+            price = weight * 2
+            print("Package sent successfully")
+            print("Package number:", random.randint(1000, 9999))
+            print(f"Price to pay: {price}$", )
+            another_operation = input("Do you want to perform another operation (y/n): ")
+            if another_operation == "y":
+                clear()
+                continue    
+            elif another_operation == "n":
+                print("Thank you for using the online shipping system!")
+                time.sleep(1)
+                break
+            time.sleep(2)
+        if choice == "2":
+            print("Thank you for using the online shipping system!")
+            time.sleep(1)
+            break
+        
+    
+
+if __name__ == "__main__":
+    main()
